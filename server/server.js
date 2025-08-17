ceb22023af3a85b6787e93ec991f7f4b3f92a109
ima-69 <imansha.idr@gmail.com>
2025-08-17 22:40:00 +0530

    Setting up project and installing dependencies

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+mongoose.connect('mongodb+srv://imanshaidr:12345idr@cluster0.kzufgaj.mongodb.net/')
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders : [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
