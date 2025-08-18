8ddd6f60f88583f5f9bf13b91a2310aba551e9a5
ima-69 <imansha.idr@gmail.com>
2025-08-18 22:14:08 +0530

    Implemented auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,28 +13,50 @@ import ShoppingHome from "./pages/shopping-view/home.jsx";
 import ShoppingListing from "./pages/shopping-view/listing.jsx";
 import ShoppingAccount from "./pages/shopping-view/account.jsx";
 import ShoppingCheckout from "./pages/shopping-view/checkout.jsx";
+import CheckAuth from "./components/common/check-auth.jsx";
+import UnauthPage from "./pages/unauth-page/index.jsx";
 
 function App() {
+
+  const isAuthenticated = true; // Replace with actual authentication logic
+  const user = {
+    name: "John Doe",
+    email: "john@example.com",
+    role: "user"
+  }; // Replace with actual user data
+
   return (
     <div className="flex flex-col overflow-hidden bg-white ">
-    
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="features" element={<AdminFeatures />} />
           <Route path="orders" element={<AdminOrders />} /> 
         </Route>
-        <Route path="/shopping" element={<ShoppingLayout/>}>
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout />
+          </CheckAuth>
+        }>
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
         </Route>
+        <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
---
+++ client/src/pages/unauth-page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnauthPage = () => {
+  return (
+    <h1 className="text-center text-2xl font-bold mt-10">
+        You are not authorized to view this page.
+    </h1>
+  )
+}
+
+export default UnauthPage
