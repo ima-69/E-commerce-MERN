2d266678e59a15881536612a4ff080ae155288c3
ima-69 <imansha.idr@gmail.com>
2025-08-20 01:24:44 +0530

    Admin view header and sidebar component completed

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import AdminSidebar from './sidebar'
-import AdminHeader from './header'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import AdminSidebar from "./sidebar";
+import AdminHeader from "./header";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
-    <div className='flex min-h-screen w-full'>
+    <div className="flex min-h-screen w-full">
       {/* admin sidebar */}
-      <AdminSidebar/>
-      <div className='flex flex-1 flex-col'>
+      <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
+      <div className="flex flex-1 flex-col">
         {/* admin header */}
-        <AdminHeader/>
-        <main className='flex-1 flex bg-muted/40 p-4 md:p-6'>
-            <Outlet/>
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
         </main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
