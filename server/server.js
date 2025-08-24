ad3abb0f3df6074698211bb2232c6e82818a087d
ima-69 <imansha.idr@gmail.com>
2025-08-24 11:57:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,13 +8,17 @@ const cors = require('cors');
 dotenv.config();
 
 const authRouter = require("./routes/auth/auth-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+
 const adminProductRouter = require("./routes/admin/products-routes");
+
 const shopProductRouter = require("./routes/shop/products-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
 
 const commonFeatureRouter = require("./routes/common/feature-routes");
-const shopAddressRouter = require("./routes/shop/address-routes");
 
 mongoose.connect(process.env.mongodbURI)
     .then(() => console.log('MongoDB connected'))
@@ -40,13 +44,19 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
+
 app.use("/api/admin/products", adminProductRouter);
+app.use("/api/admin/orders", adminOrderRouter);
+
 app.use("/api/shop/products", shopProductRouter);
 app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/shop/cart", shopCartRouter);
-app.use("/api/common/feature", commonFeatureRouter);
 app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
+
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
